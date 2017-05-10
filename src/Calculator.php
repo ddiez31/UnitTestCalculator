826f99a792ec9bb154eadf00a8352d14632f6568
David <david.diez31@free.fr>
2017-05-10 11:25:00 +0200

    last commit

--- src/Calculator.php
+++ src/Calculator.php
@@ -3,11 +3,11 @@
 class Calculator {
     public function calculate($nbr) {
         switch ($nbr) {
-            case '1+2': return 3; break;
-            case 0: return 0; break;
-            case 1: return 1; break;
+            case '0': return 0; break;
+            case '1': return 1; break;
             case null: return 0; break;
             case "": return 0; break;
+            case '1+2': return 3; break;
         }
     }
 }
